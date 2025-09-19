65d335058e03f3fb3fdab1719a3c5bb24fb499f7
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-09-19 23:03:24 +0530

    pushing to phe github for first time

---
+++ src/main/java/com/Kamasn/gharKaBawarchi/GharKaBawarchiApplication.java
@@ -0,0 +1,13 @@
+package com.Kamasn.gharKaBawarchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GharKaBawarchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GharKaBawarchiApplication.class, args);
+	}
+
+}
