05f9f4e6da5291ee8dd5fbee68d6e082793f1c81
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-09-27 22:48:32 +0530

    creating the controller

---
+++ src/main/java/com/Kamran/gharKaBawarchi/Controller/User/UserController.java
@@ -0,0 +1,60 @@
+package com.Kamran.gharKaBawarchi.Controller.User;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.Kamran.gharKaBawarchi.Dto.LogInDto;
+import com.Kamran.gharKaBawarchi.Entity.Cook;
+import com.Kamran.gharKaBawarchi.Entity.Users;
+import com.Kamran.gharKaBawarchi.Service.CookService;
+import com.Kamran.gharKaBawarchi.Service.UserService;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    @Autowired
+    private final CookService cookService;
+
+    public UserController(UserService userService, CookService cookService) {
+        this.userService = userService;
+        this.cookService=cookService;
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model){
+        model.addAttribute("loginDto", new LogInDto());
+        return "login";
+    }
+
+    @PostMapping("/home")
+    public String procesLogin(LogInDto logInDto, Model model, RedirectAttributes redirectAttributes){
+        System.out.println("********************************");
+        System.out.println(logInDto.getUserName());
+        System.out.println(logInDto.getPassword());
+        Boolean isValid=userService.processLogin(logInDto);
+        System.out.println("*****************************");
+        System.out.println(isValid);
+        if(isValid){
+            Users user=userService.getUser(logInDto.getUserName());
+            System.out.println(logInDto.getUserName());
+            model.addAttribute("user", user);
+            List<Cook> cooks=cookService.getAllCookByCity(user.getCity());
+            model.addAttribute("cooks", cooks);
+
+            return "user_dashboard";
+        }
+        return "redirect:/login?error=true";
+    }
+
+}
---
+++ src/main/java/com/Kamran/gharKaBawarchi/Entity/Addres.java
@@ -0,0 +1,30 @@
+package com.Kamran.gharKaBawarchi.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Addres {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+    // @OneToOne(mappedBy = "addres")
+    // private Users user;
+}
