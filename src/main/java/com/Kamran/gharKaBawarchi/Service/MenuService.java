8b1cb29bd459e77fe72832e373f4cc100e6adde6
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-10-03 00:53:28 +0530

    adding booking form

---
+++ src/main/java/com/Kamran/gharKaBawarchi/Service/MenuService.java
@@ -0,0 +1,22 @@
+package com.Kamran.gharKaBawarchi.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Kamran.gharKaBawarchi.Entity.Cook;
+import com.Kamran.gharKaBawarchi.Entity.Menu;
+import com.Kamran.gharKaBawarchi.Respository.MenuRepository;
+
+@Service
+public class MenuService {
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    public List<Menu> getMenuByCook(Cook cook) {
+        return menuRepository.findByCook(cook);
+        
+    }
+}
