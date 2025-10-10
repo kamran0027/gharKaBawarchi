1e6ff649868cc7a989925b09ed483333382cbd0e
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-10-11 01:08:07 +0530

    changing the address entity name

---
+++ src/main/java/com/Kamran/gharKaBawarchi/Entity/Address.java
@@ -0,0 +1,30 @@
+package com.Kamran.gharKaBawarchi.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    @OneToOne(mappedBy = "address")
+    private Users user;
+}
