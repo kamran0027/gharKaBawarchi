7bb385981b1a7a1df27094728c079724d9d56726
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-10-17 00:46:39 +0530

    implementing change the city service

--- src/main/java/com/Kamran/gharKaBawarchi/Service/CityService.java
+++ src/main/java/com/Kamran/gharKaBawarchi/Service/CityService.java
@@ -1,21 +1,38 @@
 package com.Kamran.gharKaBawarchi.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Kamran.gharKaBawarchi.Entity.City;
+import com.Kamran.gharKaBawarchi.Entity.Users;
 import com.Kamran.gharKaBawarchi.Respository.CityRepository;
+import com.Kamran.gharKaBawarchi.Respository.UserRepository;
 
 @Service
 public class CityService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private CityRepository cityRepository;
 
-    public void updateCity(String cityName){
-        cityRepository.findByCityName(cityName);
+    public boolean updateCity(Long cityId){
+        Optional<City> city= cityRepository.findById(cityId);
+        if (city.isPresent()) {
+            Users user=userService.getUser("kamranahmadmd463@gmail.com");
+            if(user!=null){
+                user.setCity(city.get());
+                userRepository.save(user);
+                return true;
+            }
+        }
+        return false;
     }
     public List<City> getAllCitys(){
         return cityRepository.findAll();
