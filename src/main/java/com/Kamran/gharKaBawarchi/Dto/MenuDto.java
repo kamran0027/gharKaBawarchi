ea27cbb223545d8edbb539d3154d1f602f61ee51
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-11-09 16:32:36 +0530

    implementing add menu item on the cook side and they can set their price

---
+++ src/main/java/com/Kamran/gharKaBawarchi/Dto/MenuDto.java
@@ -0,0 +1,16 @@
+package com.Kamran.gharKaBawarchi.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MenuDto {
+    private String menuItemName;
+    private double price;
+
+}
